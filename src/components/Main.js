3b13ea5d7244af36c695cfd97960d09b4cbb8ab7
Nashit Shayan <shayan.nashit@gmail.com>
2022-04-27 16:10:00 +0530

    form added

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,27 @@
 import React from 'react';
-
+import images from '../importImages';
 function Main() {
-	return <main className='main'>Main</main>;
+	return (
+		<main className='main'>
+			<form className='form'>
+				<div>
+					<input
+						type='text'
+						placeholder='meme header'
+						className='form--input'
+					/>
+					<input
+						type='text'
+						placeholder='meme content'
+						className='form--input'
+					/>
+				</div>
+				<button className='form--submit-btn'>
+					<img src={images['meme-img.svg']} alt='get new meme' />
+				</button>
+			</form>
+		</main>
+	);
 }
 
 export default Main;
