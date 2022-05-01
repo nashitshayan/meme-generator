7859e0c203ae5937a7d6dd969850f14da785256d
Nashit Shayan <shayan.nashit@gmail.com>
2022-05-01 16:49:35 +0530

    generating memes

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,25 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import images from '../importImages';
+import memesData from '../memesData';
 function Main() {
+	const [meme, setMeme] = useState({
+		topText: '',
+		bottomText: '',
+		randomImage: 'http://i.imgflip.com/1bij.jpg',
+	});
+
+	const [allMemeImages, setAllMemeImages] = useState(memesData);
+
+	const getMemeImage = () => {
+		const memesArray = allMemeImages.data.memes;
+		const randomNumber = Math.floor(Math.random() * memesArray.length);
+		const url = memesArray[randomNumber].url;
+		setMeme((prevMeme) => {
+			return {
+				...prevMeme,
+				randomImage: url,
+			};
+		});
+	};
+
 	return (
 		<main className='main'>
-			<form className='form'>
+			<div className='form'>
 				<div>
 					<input
 						type='text'
-						placeholder='meme header'
+						placeholder='Top text'
 						className='form--input'
+						value={meme.topText}
 					/>
 					<input
 						type='text'
-						placeholder='meme content'
+						placeholder='Bottom text'
 						className='form--input'
+						value={meme.bottomText}
 					/>
 				</div>
 				<button className='form--submit-btn'>
-					<img src={images['meme-img.svg']} alt='get new meme' />
+					<img
+						src={images['meme-img.svg']}
+						alt='get new meme'
+						onClick={getMemeImage}
+					/>
 				</button>
-			</form>
+			</div>
+			<img src={meme.randomImage} alt='meme' />
 		</main>
 	);
 }
